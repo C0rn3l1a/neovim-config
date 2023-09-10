cbce7ffd03744cc742bb834879ba7eceb7e0d955
C0rn3l1a <nachoborello97@gmail.com>
2023-09-10 15:29:07 +1000

    add find buffer

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,4 +6,6 @@ wk.register({
     ["<leader>ff"] = { builtin.find_files, "Find Files" },
     ["<leader>fa"] = { builtin.live_grep, "Find Everywhere | Live Grep" },
     ["<leader>fg"] = { builtin.git_files, "Find in Git repo" },
+    ["<leader>fb"] = { builtin.buffers, "Find Buffers" },
+    ["<leader>fh"] = { builtin.help_tags, "Find Help Tags" },
 })
